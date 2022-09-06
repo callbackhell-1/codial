9fef605356bef263e388ee018516caf2d107c176
epitome-7 <jha01adarsh@gmail.com>
2022-09-07 01:22:39 +0530

    Added comments

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,4 +1,7 @@
-module.exports.home = function(req,res){
-    return res.end("<h1>Express is Up & running for codial</h1>")
+module.exports.home = function (req, res) {
+  return res.end("<h1>Express is Up & running for codial</h1>");
 };
 
+// module.exports.profile = function (req, res) {
+//   return res.end("<h1>Profile page is Loades</h1>");
+// };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
-
 const router = express.Router();
-const homeController = require("../controllers/home_controllers");
 
-router.get("/",homeController.home);
+const homeController = require("../controllers/home_controllers");
+// const profileController = require("../controllers/home_controllers");
+
+// "/" is the url, router.get is similar to app.get & homecontroller action.
+router.get("/", homeController.home);
+
+// router.get("/profile", profileController.profile);
 
 console.log("Router Loaded...");
 module.exports = router;
