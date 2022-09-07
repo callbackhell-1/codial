de378f11f7fb369dc60520c5b051022f6b94d599
epitome-7 <jha01adarsh@gmail.com>
2022-09-07 12:11:10 +0530

    user controller and routes

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const homeController = require("../controllers/home_controllers");
-// const profileController = require("../controllers/home_controllers");
 
 // "/" is the url, router.get is similar to app.get & homecontroller action.
 router.get("/", homeController.home);
+router.use("/users",require("./users"));
 
-// router.get("/profile", profileController.profile);
 
 console.log("Router Loaded...");
 module.exports = router;
