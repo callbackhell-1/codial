d3f37b6fba67a4870767138a33a5aed997d809a7
epitome-7 <jha01adarsh@gmail.com>
2022-09-07 12:30:19 +0530

    Setup view

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const port = 8000;
 
 app.use("/", require("./routes"));
 
+// using view
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 app.listen(port, function (err) {
   if (err) {
     console.log(`Error : ${err}`);
--- routes/aboutus.js
+++ routes/aboutus.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const aboutController = require("../controllers/about_controller");
 
-
-router.get("/about",aboutController.about);
+router.get("/about", aboutController.about);
 
 module.exports = router;
