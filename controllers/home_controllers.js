77d51fad98773b58d51940ccd0c6c898cc527e49
epitome-7 <jha01adarsh@gmail.com>
2022-09-07 12:59:09 +0530

    Views used And render

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,5 +1,10 @@
 module.exports.home = function (req, res) {
-  return res.end("<h1>Express is Up & running for codial</h1>");
+  // return res.end("<h1>Express is Up & running for codial</h1>");
+
+  // return view file
+  return res.render("home", {
+    title: "Home",
+  });
 };
 
 // module.exports.profile = function (req, res) {
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,11 @@
 module.exports.profile = function (req, res) {
-  return res.end("<h1>User Profile</h1> ");
+  // return res.end("<h1>User Profile</h1> ");
+
+  // rendering view
+
+  return res.render("users",{
+    title:"profile page"
+  });
+
+  // users --> view file name.
 };
