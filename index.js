172132f48c98c076b3a8abdbf71bf8c2463ef588
epitome-7 <jha01adarsh@gmail.com>
2022-09-08 00:59:33 +0530

    Layout Implementation

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const expressLayouts = require("express-ejs-layouts");
+
+// layout
+app.use(expressLayouts);
 
 // use express router
-
 app.use("/", require("./routes"));
 
 // using view
