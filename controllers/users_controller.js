bef508b30dbfd13189efa2cc1aea9c58fb35842b
epitome-7 <jha01adarsh@gmail.com>
2022-09-08 16:36:17 +0530

    Rendering page for sign in & sign up page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,11 +1,20 @@
 module.exports.profile = function (req, res) {
-  // return res.end("<h1>User Profile</h1> ");
-
   // rendering view
-
-  return res.render("users_profile",{
-    title:"profile page"
+  return res.render("users_profile", {
+    title: "profile page",
   });
+};
 
-  // users --> view file name.
+// sign up controller  & render sign up pg
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "codial | signup",
+  });
+};
+
+// sign in controller & render sign in pg
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "codial | signIn",
+  });
 };
--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,8 @@ const userController = require("../controllers/users_controller");
 
 router.get("/profile", userController.profile);
 
+router.get("/sign-up",userController.signUp);
+
+router.get("/sign-in",userController.signIn);
+
 module.exports = router;
