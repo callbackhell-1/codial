55544ed587b625d8ae2061187cd800598e4aa8a1
epitome-7 <jha01adarsh@gmail.com>
2022-10-16 01:53:56 +0530

    setting up scss

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ const db = require("./config/mongoose");
 const session = require("express-session");
 const passport = require("./config/passport-local-strategy");
 const MongoStore = require("connect-mongodb-session")(session);
+const sassMiddleware = require("node-sass-middleware");
+
+app.use(
+  sassMiddleware({
+    src: "/assets/scss",
+    dest: "/assets/css",
+    debug: true,
+    outputStyle: "extended",
+    prefix: "/css",
+  })
+);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
