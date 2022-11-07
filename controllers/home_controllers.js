7e26ea8101dd34410dd92b2368b327bb44006ec6
Adarsh <jha01adarsh@gmail.com>
2022-11-07 23:00:11 +0530

    Display post and related user

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -5,12 +5,22 @@ module.exports.home = function (req, res) {
   // console.log(req.cookies);
   // res.cookie("user_id", 222);
   // return view file
-  Post.find({}, function (err, posts) {
-    return res.render("home", {
-      title: "Codial | Home",
-      posts: posts,
+  // return id
+  // Post.find({}, function (err, posts) {
+  //   return res.render("home", {
+  //     title: "Codial | Home",
+  //     posts: posts,
+  //   });
+  // });
+  // populate the user of each post
+  Post.find({})
+    .populate("user")
+    .exec(function (err, posts) {
+      return res.render("home", {
+        title: "Codial | Home",
+        posts: posts,
+      });
     });
-  });
 };
 
 // module.exports.profile = function (req, res) {
