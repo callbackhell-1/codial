bd40d0591fc03d0ea4d233fc51a67b9c6700854b
Adarsh <jha01adarsh@gmail.com>
2022-12-03 03:18:18 +0530

    Added the comment in code

--- models/post.js
+++ models/post.js
@@ -6,17 +6,20 @@ const postSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    // post linking to user & need to refer to user schema. ObjectId in robo3t & always unique.
+    // ref : refer to which schema.
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
-     // include the array of id's of all the comments in this post schema itself
-     comments: [
+    // when we loading the post we need to require all the comment on the post.
+    // include the array of id's of all the comments in this post schema itself
+    comments: [
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "comment",
-      }
-     ]
+      },
+    ],
   },
   {
     timestamps: true,
