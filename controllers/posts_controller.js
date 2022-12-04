581f357a12ff5b45f73d04b4eaa77ac09e1f1bad
Adarsh <jha01adarsh@gmail.com>
2022-12-05 02:56:46 +0530

    Deleting podt from DB

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,16 +1,38 @@
 const Post = require("../models/post");
+const Comment = require("../models/comment");
 
 module.exports.create = function (req, res) {
-  Post.create({
-    content: req.body.content,
-    user:req.user._id,
-  },function(err,post){
-    if(err){
+  Post.create(
+    {
+      content: req.body.content,
+      user: req.user._id,
+    },
+    function (err, post) {
+      if (err) {
         console.log("error in creating post");
-        return ;
+        return;
+      }
+      return res.redirect("back");
     }
-    return res.redirect("back");
-  }
-  
   );
+};
+
+module.exports.destroy = function (req, res) {
+  //  Before deleting the post check post exist or not
+  Post.findById(req.params.id, function (err, post) {
+if (post.user == req.user.id) {
+      post.remove();
+
+      Comment.deleteMany({ post: req.params.id }, function (err) {
+        if (err) {
+          console.log("Error in deleting comment");
+        }
+        return res.redirect("back");
+      });
+    }
+    // if (post.user == req.user.id) not match
+    else{
+      return res.redirect('back');
+  }
+  });
 };
