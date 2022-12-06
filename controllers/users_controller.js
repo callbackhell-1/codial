8fd22c9b9133ac1b6f5197d0729ec2f35ecbfeab
Adarsh <jha01adarsh@gmail.com>
2022-12-06 11:28:03 +0530

    Updating user profile

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -11,6 +11,16 @@ module.exports.profile = function (req, res) {
   });
 };
 
+module.exports.update = function (req, res) {
+  if (req.user.id == req.params.id) {
+    User.findByIdAndUpdate(req.params.id, req.body, function (err, user) {
+      return res.redirect("back");
+    });
+  } else {
+    return res.status(401).send("Unauthorized");
+  }
+};
+
 // sign up controller  & render sign up pg
 module.exports.signUp = function (req, res) {
   if (req.isAuthenticated()) {
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,13 @@ const passport = require("passport");
 const router = express.Router();
 const userController = require("../controllers/users_controller");
 
-router.get("/profile/:id", passport.checkAuthentication, userController.profile);
+router.get(
+  "/profile/:id",
+  passport.checkAuthentication,
+  userController.profile
+);
+
+router.post("/update/:id", passport.checkAuthentication, userController.update);
 
 router.get("/sign-up", userController.signUp);
 
